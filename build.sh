e732c5620b195086ebd6c57ba9d17445728d783b
Adonis255 <omondiadams05@gmail.com>
2026-06-19 01:48:45 +0300

    Use build.sh to fully control deployment and bypass automatic collectstatic

--- build.sh
+++ build.sh
@@ -2,6 +2,14 @@
 # exit on error
 set -o errexit
 
+# Disable automatic collectstatic
+export DISABLE_COLLECTSTATIC=1
+
+# Install dependencies
 pip install -r requirements.txt
+
+# Run database migrations
 python manage.py migrate
+
+# Create a superuser if it doesn't exist
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('MERCY', 'mercy254255@gmail.com', 'sucre123') if not User.objects.filter(username='MERCY').exists() else None" | python manage.py shell
